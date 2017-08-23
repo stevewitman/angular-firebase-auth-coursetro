9ef7bd7235692a5b7986557fad3638797ba3c4b3
Steve Witman <stevewitman@gmail.com>
2017-08-22 20:02:29 -0600

    Finished example ... working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgHIivntvw21287l5HXvIhqvIdN080ztY",
+  authDomain: "inventory-app-2098e.firebaseapp.com",
+  databaseURL: "https://inventory-app-2098e.firebaseio.com",
+  projectId: "inventory-app-2098e",
+  messagingSenderId: "336029239911"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
